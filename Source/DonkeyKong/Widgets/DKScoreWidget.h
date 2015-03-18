59ee4667947afffd9f3513922c900fe53e58c2c9
Lukasz "iniside" Baran <xeavien@gmail.com>
2015-03-18 10:05:57 +0100

    added blueprint versions of classes, and basic UI

---
+++ Source/DonkeyKong/Widgets/DKScoreWidget.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Blueprint/UserWidget.h"
+#include "DKScoreWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DONKEYKONG_API UDKScoreWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};

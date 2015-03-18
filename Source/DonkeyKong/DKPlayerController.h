575f7465c9cb2fac6ecef4f013afb6ece1e8016e
Lukasz "iniside" Baran <xeavien@gmail.com>
2015-03-18 10:04:54 +0100

    added base class for custom player controller

---
+++ Source/DonkeyKong/DKPlayerController.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/PlayerController.h"
+#include "DKPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DONKEYKONG_API ADKPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};

380381c05e576f75201e7d9f560bcd147700aec7
Lukasz "iniside" Baran <xeavien@gmail.com>
2015-03-18 10:04:15 +0100

    added dependencies to support creating UMG widgets in code

--- Source/DonkeyKong/DonkeyKong.Build.cs
+++ Source/DonkeyKong/DonkeyKong.Build.cs
@@ -6,6 +6,15 @@ public class DonkeyKong : ModuleRules
 {
 	public DonkeyKong(TargetInfo Target)
 	{
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] 
+        { 
+            "Core", 
+            "CoreUObject", 
+            "Engine", 
+            "InputCore",
+            "UMG",
+            "Slate",
+            "SlateCore"
+        });
 	}
 }
--- Source/DonkeyKong/DonkeyKong.h
+++ Source/DonkeyKong/DonkeyKong.h
@@ -4,5 +4,11 @@
 #define __DONKEYKONG_H__
 
 #include "Engine.h"
-				   
+
+#include "Runtime/UMG/Public/UMG.h"
+#include "Runtime/UMG/Public/UMGStyle.h"
+#include "Runtime/UMG/Public/Slate/SObjectWidget.h"
+#include "Runtime/UMG/Public/IUMGModule.h"
+#include "Runtime/UMG/Public/Blueprint/UserWidget.h"
+
 #endif

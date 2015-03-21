301b9468e4f1b50eb234304c9ea818989bc8d8e2
Lukasz "iniside" Baran <xeavien@gmail.com>
2015-03-21 14:18:57 +0100

    fixed blueprint to match new classes

---
+++ Source/DonkeyKong/DKGlobalTypes.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+//#include "DKGlobalTypes.generated.h"
+
+//UENUM()
+namespace ENavAreaFlag
+{
+	enum Type
+	{
+		Default,
+		Climb
+	};
+}
+//UENUM()
+//enum class ENavAreaFlag
+//{
+//	Default,
+//	Climb
+//};

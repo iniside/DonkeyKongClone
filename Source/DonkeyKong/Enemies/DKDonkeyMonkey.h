a3d12a57911d6525791ac9e565653549939d9830
Lukasz "iniside" Baran <xeavien@gmail.com>
2015-03-25 20:35:12 +0100

    added comment describing class

--- Source/DonkeyKong/Enemies/DKDonkeyMonkey.h
+++ Source/DonkeyKong/Enemies/DKDonkeyMonkey.h
@@ -1,12 +1,16 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
-
+#include "DKEnemy.h"
 #include "GameFramework/Pawn.h"
 #include "DKDonkeyMonkey.generated.h"
 
+/*
+	Placeholder for Monkey which does look like Donkey.
+*/
+
 UCLASS()
-class DONKEYKONG_API ADKDonkeyMonkey : public APawn
+class DONKEYKONG_API ADKDonkeyMonkey : public ADKEnemy
 {
 	GENERATED_BODY()
 
